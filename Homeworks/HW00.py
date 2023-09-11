ce5bbcafe3a15a5d3ab96e643d5a9545fe64e123
zchen125 <zchen125@stevens.edu>
2023-09-11 10:28:28 -0400

    HW00

---
+++ Homeworks/HW00.py
@@ -0,0 +1,16 @@
+#HW 00 - Tools Setup
+
+from datetime import date
+
+def my_brand(): 
+    print("\n=*=*=*= Your name =*=*=*=\n")
+    print("=*=*=*= Course 2023S-SSW567-WS =*=*=*= \n")
+    print("=*=*=*= HW 00 - Tools Setup =*=*=*=\n") 
+    print("=*=*=*= ", date.today() ," =*=*=*= \n")
+
+
+my_brand() ## START
+
+print("Hello world")
+
+my_brand() ## END
