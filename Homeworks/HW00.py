b47720e7392e2419a54707cc2dc9c3cbcc8417d1
zchen125 <zchen125@stevens.edu>
2023-09-12 18:47:05 -0400

    add HW01 and revise HW00

--- Homeworks/HW00.py
+++ Homeworks/HW00.py
@@ -3,7 +3,7 @@
 from datetime import date
 
 def my_brand(): 
-    print("\n=*=*=*= Your name =*=*=*=\n")
+    print("\n=*=*=*= Zuting Chen =*=*=*=\n")
     print("=*=*=*= Course 2023S-SSW567-WS =*=*=*= \n")
     print("=*=*=*= HW 00 - Tools Setup =*=*=*=\n") 
     print("=*=*=*= ", date.today() ," =*=*=*= \n")
