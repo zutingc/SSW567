f458361bacc9c1e300da40b9cc2f3ffc8a7ffe86
zchen125 <zchen125@stevens.edu>
2023-09-26 02:57:28 -0400

    modify test triangle for hw02a

--- Homeworks/HW02a/Triangle.py
+++ Homeworks/HW02a/Triangle.py
@@ -31,7 +31,7 @@ def classifyTriangle(a,b,c):
     if a > 200 or b > 200 or c > 200:
         return 'InvalidInput'
         
-    if a <= 0 or b <= b or c <= 0:
+    if a <= 0 or b <= 0 or c <= 0:
         return 'InvalidInput'
     
     # verify that all 3 inputs are integers  
@@ -43,15 +43,15 @@ def classifyTriangle(a,b,c):
     # is important for correctness
     # the sum of any two sides must be strictly less than the third side
     # of the specified shape is not a triangle
-    if (a >= (b - c)) or (b >= (a - c)) or (c >= (a + b)):
+    if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)):
         return 'NotATriangle'
         
     # now we know that we have a valid triangle 
     if a == b and b == a:
         return 'Equilateral'
-    elif ((a * 2) + (b * 2)) == (c * 2):
+    elif (pow(a,2) + pow(b,2) == pow(c,2)) or (pow(b,2) + pow(c,2) == pow(a,2)) or (pow(a,2) + pow(c,2) == pow(b,2)):
         return 'Right'
-    elif (a != b) and  (b != c) and (a != b):
+    elif (a != b) and  (b != c) and (a != c):
         return 'Scalene'
     else:
         return 'Isoceles'
