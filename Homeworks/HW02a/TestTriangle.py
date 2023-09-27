0fe23fb047d1936d6eed58dc3352c0831876b2c5
zchen125 <zchen125@stevens.edu>
2023-09-26 23:25:58 -0400

    add more unit tests

--- Homeworks/HW02a/TestTriangle.py
+++ Homeworks/HW02a/TestTriangle.py
@@ -24,8 +24,30 @@ class TestTriangles(unittest.TestCase):
     def testRightTriangleB(self): 
         self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
         
+    def testRightTriangleC(self): 
+        self.assertEqual(classifyTriangle(4,5,3),'Right','4,5,3 is a Right triangle')
+
     def testEquilateralTriangles(self): 
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+
+    def testIsoceles(self): 
+        self.assertEqual(classifyTriangle(2,3,2),'Isoceles','2,3,2 is a Isoceles triangle')
+
+    def testScalene(self): 
+        self.assertEqual(classifyTriangle(3,7,6),'Scalene','1,0,1 is a Scalene triangle')
+
+    def testInvalidInput(self): 
+        self.assertEqual(classifyTriangle(1,0,1),'InvalidInput','1,0,1 is not a valid input')
+
+    def testInvalidInput2(self): 
+        self.assertEqual(classifyTriangle(1,201,1),'InvalidInput','1,201,1 is not a valid input')
+
+    def testInvalidInput3(self): 
+        self.assertEqual(classifyTriangle(201,1,1),'InvalidInput','201,1,1 is not a valid input')
+
+    def testNotATriangle(self): 
+        self.assertEqual(classifyTriangle(1,100,1),'NotATriangle','1,100,1 should not be a triangle')
+
 
 my_brand() ## START
 
